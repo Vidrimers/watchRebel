5cb54d68df373b3df0eb80576ec6f64c42e9af49
Vidrimers <vidrimers2@gmail.com>
2026-02-16 19:51:19 +0300

    feat: настроен роутинг с lazy loading для всех страниц

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAppSelector } from './hooks/useAppSelector';
 import AppInitializer from './components/AppInitializer';
-import LoginPage from './pages/LoginPage';
-import UserProfilePage from './pages/UserProfilePage';
-import SearchPage from './pages/SearchPage';
-import MediaDetailPage from './pages/MediaDetailPage';
-import ListsPage from './pages/ListsPage';
-import WatchlistPage from './pages/WatchlistPage';
 import ProtectedRoute from './components/ProtectedRoute';
+
+// Lazy loading для всех страниц
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const UserProfilePage = lazy(() => import('./pages/UserProfilePage'));
+const SearchPage = lazy(() => import('./pages/SearchPage'));
+const MediaDetailPage = lazy(() => import('./pages/MediaDetailPage'));
+const ListsPage = lazy(() => import('./pages/ListsPage'));
+const WatchlistPage = lazy(() => import('./pages/WatchlistPage'));
+const NotificationsPage = lazy(() => import('./pages/NotificationsPage'));
+const SettingsPage = lazy(() => import('./pages/SettingsPage'));
+
+// Компонент загрузки для Suspense
+function LoadingFallback() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '18px',
+      color: '#666'
+    }}>
+      Загрузка...
+    </div>
+  );
+}
 
 // Временный компонент главной страницы - редирект на профиль пользователя
 function HomePage() {
@@ -25,73 +45,95 @@ function App() {
   return (
     <BrowserRouter>
       <AppInitializer>
-        <Routes>
-          {/* Публичный маршрут - страница логина */}
-          <Route path="/login" element={<LoginPage />} />
-          
-          {/* Защищенные маршруты */}
-          <Route 
-            path="/" 
-            element={
-              <ProtectedRoute>
-                <HomePage />
-              </ProtectedRoute>
-            } 
-          />
+        <Suspense fallback={<LoadingFallback />}>
+          <Routes>
+            {/* Публичный маршрут - страница логина */}
+            <Route path="/login" element={<LoginPage />} />
+            
+            {/* Защищенные маршруты */}
+            <Route 
+              path="/" 
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              } 
+            />
 
-          {/* Страница профиля пользователя */}
-          <Route 
-            path="/user/:userId" 
-            element={
-              <ProtectedRoute>
-                <UserProfilePage />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Страница профиля пользователя */}
+            <Route 
+              path="/user/:userId" 
+              element={
+                <ProtectedRoute>
+                  <UserProfilePage />
+                </ProtectedRoute>
+              } 
+            />
 
-          {/* Страница поиска */}
-          <Route 
-            path="/search" 
-            element={
-              <ProtectedRoute>
-                <SearchPage />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Страница поиска */}
+            <Route 
+              path="/search" 
+              element={
+                <ProtectedRoute>
+                  <SearchPage />
+                </ProtectedRoute>
+              } 
+            />
 
-          {/* Детальная страница медиа-контента */}
-          <Route 
-            path="/media/:mediaType/:mediaId" 
-            element={
-              <ProtectedRoute>
-                <MediaDetailPage />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Детальная страница медиа-контента */}
+            <Route 
+              path="/media/:mediaType/:mediaId" 
+              element={
+                <ProtectedRoute>
+                  <MediaDetailPage />
+                </ProtectedRoute>
+              } 
+            />
 
-          {/* Страница списков */}
-          <Route 
-            path="/lists" 
-            element={
-              <ProtectedRoute>
-                <ListsPage />
-              </ProtectedRoute>
-            } 
-          />
+            {/* Страница списков */}
+            <Route 
+              path="/lists" 
+              element={
+                <ProtectedRoute>
+                  <ListsPage />
+                </ProtectedRoute>
+              } 
+            />
 
-          {/* Страница списка желаемого */}
-          <Route 
-            path="/watchlist" 
-            element={
-              <ProtectedRoute>
-                <WatchlistPage />
-              </ProtectedRoute>
-            } 
-          />
-          
-          {/* Перенаправление всех неизвестных маршрутов на главную */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+            {/* Страница списка желаемого */}
+            <Route 
+              path="/watchlist" 
+              element={
+                <ProtectedRoute>
+                  <WatchlistPage />
+                </ProtectedRoute>
+              } 
+            />
+
+            {/* Страница уведомлений */}
+            <Route 
+              path="/notifications" 
+              element={
+                <ProtectedRoute>
+                  <NotificationsPage />
+                </ProtectedRoute>
+              } 
+            />
+
+            {/* Страница настроек */}
+            <Route 
+              path="/settings" 
+              element={
+                <ProtectedRoute>
+                  <SettingsPage />
+                </ProtectedRoute>
+              } 
+            />
+            
+            {/* Перенаправление всех неизвестных маршрутов на главную */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
       </AppInitializer>
     </BrowserRouter>
   );
---
+++ client/src/routes/index.jsx
@@ -0,0 +1,78 @@
+import React, { lazy } from 'react';
+import { Navigate } from 'react-router-dom';
+import ProtectedRoute from '../components/ProtectedRoute';
+
+// Lazy loading для всех страниц
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const UserProfilePage = lazy(() => import('../pages/UserProfilePage'));
+const SearchPage = lazy(() => import('../pages/SearchPage'));
+const MediaDetailPage = lazy(() => import('../pages/MediaDetailPage'));
+const ListsPage = lazy(() => import('../pages/ListsPage'));
+const WatchlistPage = lazy(() => import('../pages/WatchlistPage'));
+const NotificationsPage = lazy(() => import('../pages/NotificationsPage'));
+const SettingsPage = lazy(() => import('../pages/SettingsPage'));
+
+/**
+ * Конфигурация маршрутов приложения
+ * Все маршруты используют lazy loading для оптимизации загрузки
+ */
+export const routes = [
+  {
+    path: '/login',
+    element: <LoginPage />,
+    protected: false,
+    title: 'Вход'
+  },
+  {
+    path: '/user/:userId',
+    element: <UserProfilePage />,
+    protected: true,
+    title: 'Профиль пользователя'
+  },
+  {
+    path: '/search',
+    element: <SearchPage />,
+    protected: true,
+    title: 'Поиск'
+  },
+  {
+    path: '/media/:mediaType/:mediaId',
+    element: <MediaDetailPage />,
+    protected: true,
+    title: 'Детали контента'
+  },
+  {
+    path: '/lists',
+    element: <ListsPage />,
+    protected: true,
+    title: 'Списки'
+  },
+  {
+    path: '/watchlist',
+    element: <WatchlistPage />,
+    protected: true,
+    title: 'Список желаемого'
+  },
+  {
+    path: '/notifications',
+    element: <NotificationsPage />,
+    protected: true,
+    title: 'Уведомления'
+  },
+  {
+    path: '/settings',
+    element: <SettingsPage />,
+    protected: true,
+    title: 'Настройки'
+  }
+];
+
+/**
+ * Обертка для защищенных маршрутов
+ */
+export function wrapRoute(route) {
+  if (route.protected) {
+    return <ProtectedRoute>{route.element}</ProtectedRoute>;
+  }
+  return route.element;
+}
