d4b07fcb7ff50f8d47c7b47a33fd39713db479c4
Vidrimers <vidrimers2@gmail.com>
2026-02-17 03:53:24 +0300

    feat: добавлена поддержка host: true в Vite для доступа по сети

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true, // Слушать на всех интерфейсах
     proxy: {
       '/api': {
         target: 'http://localhost:1313',
