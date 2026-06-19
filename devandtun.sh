6b5e1de6dd7b4a85a078a2c5086ab5254cd11564
Vidrimers <vidrimers2@gmail.com>
2026-06-19 07:43:59 +0300

    remove devandtun.sh from tracking

--- devandtun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Запуск watchRebel через tuna.am
-
-npx concurrently \
-  "tuna http 3000 --domain=dev.watchrebel.ru" \
-  "npm run dev:client" \
-  "npm run dev:server" \
-  "npm run dev:tg"
